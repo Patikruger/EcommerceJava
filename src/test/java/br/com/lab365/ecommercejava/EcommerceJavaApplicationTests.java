3ed4df3b96e39e4d927bc97e2441c2ed9cdd96d9
Patynet <patricia_kruger1@estudante.sesisenai.org.br>
2023-08-15 23:16:04 -0300

    Projeto ecommerce criado

---
+++ src/test/java/br/com/lab365/ecommercejava/EcommerceJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lab365.ecommercejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
